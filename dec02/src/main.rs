9616d4aa2a9ab0138e26a20206ef024fc6a9cba5
Ted C. Howard <ted@tedchoward.com>
2020-12-02 12:07:05 -0600

    dec02 puzzle 2

--- dec02/src/main.rs
+++ dec02/src/main.rs
@@ -1,7 +1,7 @@
 #[derive(Debug)]
 struct PasswordPolicy {
-    min: u32,
-    max: u32,
+    min: usize,
+    max: usize,
     letter: char,
 }
 
@@ -17,11 +17,7 @@ impl PasswordPolicy {
         let min = parts[0].trim().parse().unwrap();
         let max = parts[1].trim().parse().unwrap();
 
-        Self {
-            min,
-            max,
-            letter,
-        }
+        Self { min, max, letter }
     }
 }
 
@@ -43,28 +39,56 @@ impl Entry {
         Self { policy, password }
     }
 
-    fn is_valid(&self) -> bool {
-        let matches = self.password.matches(self.policy.letter).collect::<Vec<&str>>().len();
-        matches >= self.policy.min as usize && matches <= self.policy.max as usize
+    /// Counts the number of occurrences of `policy.letter` in the password.
+    /// Returns true if the count is within the range of `policy.max` -
+    /// `policy.min`
+    fn is_valid_old_rules(&self) -> bool {
+        let matches = self
+            .password
+            .matches(self.policy.letter)
+            .collect::<Vec<&str>>()
+            .len();
+        matches >= self.policy.min && matches <= self.policy.max
+    }
+
+    /// Gets a collection of 1-based indicies of all occurrences of 
+    /// `policy.letter` in `password`. Returns true if the index matches either
+    /// `policy.min` OR `policy.max`
+    fn is_valid_new_rules(&self) -> bool {
+        let matches: Vec<_> = self
+            .password
+            .match_indices(self.policy.letter)
+            .map(|tuple| tuple.0)
+            .map(|idx| idx + 1)
+            .collect();
+        matches.contains(&self.policy.min) && !matches.contains(&self.policy.max)
+            || matches.contains(&self.policy.max) && !matches.contains(&self.policy.min)
     }
 }
 
 fn load_input() -> Vec<Entry> {
     let input = std::fs::read_to_string("dec02/src/input.txt").expect("Unable to read input file");
 
-    input
-        .split('\n')
-        .map(Entry::new)
-        .collect::<Vec<Entry>>()
+    input.split('\n').map(Entry::new).collect::<Vec<Entry>>()
 }
 
 /// Count the number of entries that have valid passwords
 fn puzzle_1() -> usize {
     let input = load_input();
-    input.iter().filter(|e| e.is_valid()).count()
+    input.iter().filter(|e| e.is_valid_old_rules()).count()
+}
+
+/// Count the number of entries that have valid passwords based on the new
+/// rules
+fn puzzle_2() -> usize {
+    let input = load_input();
+    input.iter().filter(|e| e.is_valid_new_rules()).count()
 }
 
 fn main() {
     let result = puzzle_1();
-    println!("Number of valid entries: {}", result);
+    println!("Puzzle 1, Number of valid entries: {}", result);
+
+    let result = puzzle_2();
+    println!("Puzzle 2, Number of valid entries: {}", result);
 }
