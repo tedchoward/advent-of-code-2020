65471d7315a92b3bef74d8aab839a923af624a70
Ted C. Howard <ted@tedchoward.com>
2020-12-06 17:06:07 -0600

    dec06 puzzle 2

--- dec06/src/main.rs
+++ dec06/src/main.rs
@@ -5,24 +5,6 @@ fn load_input() -> String {
 }
 
 fn puzzle_1() -> usize {
-    //     let input = String::from(
-    //         "abc
-
-    // a
-    // b
-    // c
-
-    // ab
-    // ac
-
-    // a
-    // a
-    // a
-    // a
-
-    // b",
-    //     );
-
     let input = load_input();
 
     input
@@ -37,7 +19,32 @@ fn puzzle_1() -> usize {
         .sum()
 }
 
+fn puzzle_2() -> usize {
+    let input = load_input();
+
+    input
+        .split("\n\n")
+        .map(|group| {
+            group
+                .split('\n')
+                .map(|p| p.chars().collect::<HashSet<char>>())
+                .fold(None, |acc, person| -> Option<HashSet<char>> {
+                    if let Some(prev) = acc {
+                        Some(prev.intersection(&person).cloned().collect())
+                    } else {
+                        Some(person)
+                    }
+                })
+                .unwrap()
+                .len()
+        })
+        .sum()
+}
+
 fn main() {
     let result = puzzle_1();
     println!("Puzzle 1 output: {}", result);
+
+    let result = puzzle_2();
+    println!("Puzzle 2 output: {}", result);
 }
