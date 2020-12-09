41c23e35eecadd57d26aa6c52ca24ddb685c5244
Ted C. Howard <ted@tedchoward.com>
2020-12-09 11:18:35 -0600

    dec09 puzzle 2

--- dec09/src/main.rs
+++ dec09/src/main.rs
@@ -1,3 +1,5 @@
+const PREAMBLE_SIZE: usize = 25;
+
 fn load_input() -> Vec<u64> {
     let input = std::fs::read_to_string("dec09/src/input.txt").expect("Unable to read input file");
 
@@ -8,7 +10,7 @@ fn validate_number(preamble: &[u64], number: u64) -> bool {
     for i in 0..(preamble.len() - 1) {
         for j in (i + 1)..preamble.len() {
             if number == (preamble[i] + preamble[j]) {
-                return true
+                return true;
             }
         }
     }
@@ -16,19 +18,11 @@ fn validate_number(preamble: &[u64], number: u64) -> bool {
     false
 }
 
-fn puzzle_1() -> u64 {
-    // let input = [
-    //     35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576,
-    // ];
-
-    let input = load_input();
-
-    let preamble_size = 25;
-
-    for i in preamble_size..(input.len()) {
+fn find_invalid_number(input: &[u64]) -> u64 {
+    for i in PREAMBLE_SIZE..(input.len()) {
         let number = input[i];
 
-        if !validate_number(&input[(i - preamble_size)..i], number) {
+        if !validate_number(&input[(i - PREAMBLE_SIZE)..i], number) {
             return number;
         }
     }
@@ -36,7 +30,51 @@ fn puzzle_1() -> u64 {
     panic!("All numbers are valid.");
 }
 
+fn puzzle_1() -> u64 {
+    let input = load_input();
+
+    find_invalid_number(&input)
+}
+
+fn puzzle_2() -> u64 {
+    let input = load_input();
+
+    let invalid_number = find_invalid_number(&input);
+
+    for (index, num1) in input.iter().enumerate() {
+        let mut sum = *num1;
+        let mut min = sum;
+        let mut max = sum;
+
+        for num2 in input.iter().skip(index + 1) {
+            let num2 = *num2;
+            if num2 < min {
+                min = num2;
+            }
+
+            if num2 > max {
+                max = num2;
+            }
+
+            sum += num2;
+
+            if sum == invalid_number {
+                return min + max;
+            }
+
+            if sum > invalid_number {
+                break;
+            }
+        }
+    }
+
+    panic!("Encryption weakness not found.");
+}
+
 fn main() {
     let result = puzzle_1();
     println!("Puzzle 1 output: {}", result);
+
+    let result = puzzle_2();
+    println!("Puzzle 2 output: {}", result);
 }
