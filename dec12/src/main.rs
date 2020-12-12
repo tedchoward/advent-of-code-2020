3322648e135b3451ad5468ed1373047b4eaa929a
Ted C. Howard <ted@tedchoward.com>
2020-12-12 12:30:27 -0600

    dec12 puzzle 2

--- dec12/src/main.rs
+++ dec12/src/main.rs
@@ -19,15 +19,19 @@ fn normalize_direction(direction: i32) -> i32 {
     direction
 }
 
-fn puzzle_1() -> i32 {
-//     let input = String::from(
-//         "F10
-// N3
-// F7
-// R90
-// F11",
-//     );
+fn rotate_point(point: &Point, origin: &Point, angle: i32) -> Point {
+    let p = Point { x: point.x - origin.x, y: point.y - origin.y };
 
+    match angle {
+        90 => Point { x: p.y + origin.x, y: -p.x + origin.y },
+        180 => Point { x: -p.x + origin.x, y: -p.y + origin.y },
+        270 => Point { x: -p.y + origin.x, y: p.x + origin.y },
+        360 => Point { x: p.x + origin.x, y: p.x + origin.y },
+        _ => panic!("Unexpected angle: {}", angle),
+    }
+}
+
+fn puzzle_1() -> i32 {
     let input = load_input();
 
     let mut east_west = 0;
@@ -62,7 +66,54 @@ fn puzzle_1() -> i32 {
     north_south.abs() + east_west.abs()
 }
 
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+fn puzzle_2() -> i32 {
+    let input = load_input();
+
+    let mut ship_position = Point { x: 0, y: 0 };
+    let mut waypoint = Point { x: 10, y: 1 };
+
+    for instruction in input.split('\n') {
+        let action = &instruction[0..1];
+        let value: i32 = instruction[1..].parse().unwrap();
+
+        match action {
+            "N" => waypoint.y += value,
+            "S" => waypoint.y -= value,
+            "E" => waypoint.x += value,
+            "W" => waypoint.x -= value,
+            "L" => {
+                let rotation = normalize_direction(-value);
+                waypoint = rotate_point(&waypoint, &ship_position, rotation);
+            },
+            "R" => {
+                let rotation = normalize_direction(value);
+                waypoint = rotate_point(&waypoint, &ship_position, rotation);
+            }
+            "F" => {
+                let waypoint_relative = Point { x: waypoint.x - ship_position.x, y: waypoint.y - ship_position.y };
+                for _ in 0..value {
+                    ship_position.x = waypoint.x;
+                    ship_position.y = waypoint.y;
+                    waypoint.x += waypoint_relative.x;
+                    waypoint.y += waypoint_relative.y;
+                }
+            }
+            _ => panic!("Unknown action: {}", action),
+        }
+    }
+
+    ship_position.x.abs() + ship_position.y.abs()
+}
+
 fn main() {
     let result = puzzle_1();
     println!("Puzzle 1 output: {}", result);
+
+    let result = puzzle_2();
+    println!("Puzzle 2 output: {}", result);
 }
